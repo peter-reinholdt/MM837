b889000be59fe6f5e0d7b6ffb23293ffa7434dac
Peter Reinholdt <peter.reinholdt@gmail.com>
2017-11-30 15:35:39 +0100

    Input stuff is mostly finished now

---
+++ Assignment_3/src/solvers.cc
@@ -0,0 +1,8 @@
+#include <vector>
+#include "toml.h"
+
+
+void metropolis(int n_steps_therm, int n_steps_prod, int side_length, int potts_q, double beta, std::string outfile, int outfreq){};
+void cluster(int n_steps_therm, int n_steps_prod, int side_length, int potts_q, double beta, std::string outfile, int outfreq){};
+
+
