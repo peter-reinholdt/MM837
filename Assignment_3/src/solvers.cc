7c25e78559a61b337ab455b5859ffe199ba56785
Peter Reinholdt <peter.reinholdt@gmail.com>
2017-12-04 17:35:33 +0100

    More metropolis work

--- Assignment_3/src/solvers.cc
+++ Assignment_3/src/solvers.cc
@@ -2,28 +2,65 @@
 #include <time.h>
 #include "toml.h"
 #include <random>
+#include <iostream>
+
+inline int delta_function(int i, int j){
+    if (i==j){
+        return 1;
+    }else{
+        return 0;
+    }
+}
 
 inline void metropolis_sweep(std::vector<std::vector<int> >& lattice,
                              double beta,
+                             std::mt19937& gen,
                              std::uniform_int_distribution<int>& int_dist, 
-                             std::uniform_real_distribution<float>& float_dist){
-    int delta_E;
+                             std::uniform_real_distribution<double>& double_dist){
+    int delta_E, sigma_old, sigma_new;
     int L = lattice.size();
-    //first sweep through central part
-    for (int i=1; i<L-1; i++){
-        for (int j=1; j<L-1; j++){
-            //fliiip
-        }
-    }
-    //then sweep through borders
-    //j=0 and j=side_length-1
-    for (int i=0; i<L-1; i++){
+    int i_plus, i_minus, j_plus, j_minus;
+    double p_accept;
 
-    }
-    //i=0 and i=side_length-1
-    //only flip corners one time per sweep
-    for (int j=1; j<L-2; j++){
-    
+    for (int i=0; i<L; i++){
+        for (int j=0; j<L; j++){
+            i_plus  = (i != L-1) ? i+1: 0;
+            i_minus = (i != 0  ) ? i-1: L-1;
+            j_plus  = (j != L-1) ? j+1: 0;
+            j_minus = (j != 0  ) ? j-1: L-1;
+            
+            sigma_old = lattice[i][j];
+            sigma_new = int_dist(gen);
+            while (sigma_old == sigma_new){
+                sigma_new = int_dist(gen);
+            }
+            //change in energy is delta_E = E(sigma') - E(sigma)
+            //local energy is sum_{i,j} (1 - delta(i,j))
+            //but we want differences, 
+            //so in the change we don't to include the 1.
+            delta_E = 0;
+            delta_E += ( - delta_function(sigma_new, lattice[i_plus][j]));
+            delta_E += ( - delta_function(sigma_new, lattice[i_minus][j]));
+            delta_E += ( - delta_function(sigma_new, lattice[i][j_plus]));
+            delta_E += ( - delta_function(sigma_new, lattice[i][j_minus]));
+            // plus energy after flip
+            delta_E -= ( - delta_function(sigma_old, lattice[i_plus][j]));
+            delta_E -= ( - delta_function(sigma_old, lattice[i_minus][j]));
+            delta_E -= ( - delta_function(sigma_old, lattice[i][j_plus]));
+            delta_E -= ( - delta_function(sigma_old, lattice[i][j_minus]));
+            
+            //check for acceptance
+            if (delta_E <= 0){
+                //accept if energy is lower
+                lattice[i][j] = sigma_new;
+            } else {
+                p_accept = exp(-beta*delta_E);
+                if (p_accept > double_dist(gen)){
+                    //accept with p_accept probability otherwise
+                    lattice[i][j] = sigma_new;
+                }
+            }
+        }
     }
 };
 inline void compute_properties(){};
@@ -31,21 +68,40 @@ inline void compute_properties(){};
 
 void metropolis(int n_steps_therm, int n_steps_prod, int side_length, int potts_q, double beta, std::string outfile, int outfreq){
     //store lattice as 2d array
-    //65536 possible q values is probly enough.
     std::vector<std::vector<int> > lattice;
     std::mt19937 gen(time(NULL));
     std::uniform_int_distribution<int> int_dist(0, potts_q-1);
-    std::uniform_real_distribution<float> real_dist(0,1);
+    std::uniform_real_distribution<double> real_dist(0.0, 1.0);
     
+    //setup lattice (hot start)
+    for (int i=0; i<side_length; i++){
+        std::vector<int> line;
+        for (int j=0; j<side_length; j++){
+            line.push_back(int_dist(gen));     
+        }
+        lattice.push_back(line);
+    }
+
+
     //thermalize
     for (int n=0; n<n_steps_therm; n++){
-        metropolis_sweep(lattice, beta, int_dist, real_dist);
+        metropolis_sweep(lattice, beta, gen, int_dist, real_dist);
     }
     //production run
     for (int n=0; n<n_steps_prod; n++){
-        //metropolis_sweep();
-        //compute_properties();
+        metropolis_sweep(lattice, beta, gen, int_dist, real_dist);
+        if (n%outfreq == 0){
+            compute_properties();
+        }
     }
+    /*
+    for (int i=0; i<side_length; i++){
+        for (int j=0; j<side_length; j++){
+            std::cout << lattice[i][j];
+        }
+        std::cout << "\n";
+    }
+    */
 };
 
 
