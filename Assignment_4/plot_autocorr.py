09261e9208fb375ad67bf72e5b5076822d14f5d6
Peter Reinholdt <peter.reinholdt@gmail.com>
2017-12-28 11:34:59 +0100

    Added small stuff

--- Assignment_4/plot_autocorr.py
+++ Assignment_4/plot_autocorr.py
@@ -4,13 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-filename = sys.argv[1]
+filenames = sys.argv[1:]
 
-data = np.loadtxt(filename)
+data = np.zeros(np.loadtxt(filenames[0]).shape)
+
 
 plt.subplot(211)
+for fn in filenames:
+    x = np.loadtxt(fn)
+    data += x
+    plt.plot(x[:,0], x[:,1], alpha=0.1)
+data = data / len(filenames)
 plt.plot(data[:,0], data[:,1])
 plt.subplot(212)
+
+for fn in filenames:
+    x = np.loadtxt(fn)
+    plt.plot(x[:,0], x[:,2], alpha=0.1)
+
 plt.plot(data[:,0], data[:,2])
 plt.show()
-
